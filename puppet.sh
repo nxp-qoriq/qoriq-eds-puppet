afb06cba0056bf602d462eb42b058be436d16ced
Chenhui Zhao <chenhui.zhao@nxp.com>
2017-12-19 18:42:03 +0800

    fix restart problem
    Signed-off-by: Chenhui Zhao <chenhui.zhao@nxp.com>

--- puppet.sh
+++ puppet.sh
@@ -7,7 +7,16 @@
 #!/bin/bash
 
 cert_name=$(cat /etc/hostname | tr -d '\n').pem
+rundir=/var/run/puppetlabs
+certdir=/etc/ssl/private/edgescale
 
-sudo ln -s /etc/ssl/private/edgescale/certs/edgescale.pem /etc/ssl/private/edgescale/certs/$cert_name
-sudo ln -s /etc/ssl/private/edgescale/private_keys/edgescale.key /etc/ssl/private/edgescale/private_keys/$cert_name
+[ ! -h ${certdir}/certs/${cert_name} ] && [ -e ${certdir}/certs/edgescale.pem ] && \
+sudo ln -s ${certdir}/certs/edgescale.pem ${certdir}/certs/${cert_name}
+
+[ ! -h ${certdir}/private_keys/${cert_name} ] && [ -e ${certdir}/private_keys/edgescale.key ] && \
+sudo ln -s ${certdir}/private_keys/edgescale.key ${certdir}/private_keys/${cert_name}
+
+# kill puppet agent if it exists
+[ -e ${rundir}/agent.pid ] && sudo kill -s SIGINT $(<"${rundir}/agent.pid")
+
 puppet agent --daemonize $@
